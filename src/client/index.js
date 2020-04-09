cf64c83caf0420ace6cbd71eef18e22920b2e0a8
Jordan Gillard <jordan-gillard@outlook.com>
2020-04-09 16:34:52 +0200

    feat: Adds method for handling form submit event
    This commit adds the handleFormSubmit function for the client.
    At the moment, this function is able to send the destination
    and date values from the form to the backend server. The client
    still needs update the UI based on what the server returns.
    
    Related to: #9

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,7 @@
 import './styles/elements.scss';
 import './styles/scaffold.scss';
 import './styles/fonts.scss';
+import { handleFormSubmit } from './js/handleFormSubmit'
+require("regenerator-runtime/runtime");
+
+export { handleFormSubmit }
---
+++ src/client/js/handleFormSubmit.js
@@ -0,0 +1,28 @@
+async function handleFormSubmit (event) {
+    event.preventDefault();
+    const location = document.getElementById('dest-input');
+    const date = document.getElementById('departure-date');
+    const data = {
+        location: location.value,
+        date: date.value
+    };
+
+    console.log('Sending data to /post-trip-info: ', data);
+
+    const serverEndpoint = 'http://localhost:3001/post-trip-info'
+    const response = await fetch(serverEndpoint, {
+        method: 'POST',
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+export { handleFormSubmit }
